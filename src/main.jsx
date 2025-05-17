9612a0573c1624f1893e2fbbf4bc887256753fb9
tanmay-sketch <grandhi1@msu.edu>
2025-05-17 18:45:59 +0530

    homepage and initial board

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { StyledEngineProvider } from '@mui/material/styles';
+import GlobalStyles from '@mui/material/GlobalStyles';
 import './index.css'
 import App from './App.jsx'
+import { ThemeProvider } from './context/ThemeContext';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ThemeProvider>
+      <StyledEngineProvider enableCssLayer>
+        <GlobalStyles styles="@layer theme, base, mui, components, uitilities" />
+        <App />
+      </StyledEngineProvider>
+    </ThemeProvider>
   </StrictMode>,
 )
